3eb1e09c593b91b14b44cecebcd59468902096d3
Vladimir612 <vladimir.12.lazarevic@gmail.com>
2020-11-27 16:10:27 +0100

    Added about part, sound, animations on first page, changed loading.css and rotate.css to sass files

--- public/frontend/js/first.js
+++ public/frontend/js/first.js
@@ -1,3 +1,19 @@
 //Redirect to mode page
 const startButtonFirst = document.querySelector('.btn-start');
 startButtonFirst.addEventListener('click', () => window.location.replace("mode.html"));
+//Modal
+const btnAbout = document.querySelector('.btn-info');
+btnAbout.addEventListener('click', openModal);
+
+const modalAbout = document.querySelector('.modal');
+const closeX = document.querySelector('.close-x');
+
+closeX.addEventListener('click', closeModal);
+
+function openModal(){
+    modalAbout.classList.remove('modal-hidden');
+}
+
+function closeModal(){
+    modalAbout.classList.add('modal-hidden');
+}
